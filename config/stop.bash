407d854a8fdeffbbe27da45c06146df5bddb879d
Joseph Surmava <joseph@worldeffica.com>
2017-02-19 15:32:38 -0700

    Wrote an example of assigning click event handler in pure JS. Made nginx use CodeKit web server. Fixed config/stop.bash.

--- config/stop.bash
+++ config/stop.bash
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # https://www.nginx.com/resources/wiki/start/topics/tutorials/commandline/
-kill -QUIT "$(< logs/nginx.pid)"
+# http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+dir="$(cd "$(dirname "$0")" && cd .. && pwd)"
+# https://www.nginx.com/resources/wiki/start/topics/tutorials/commandline/
+kill -QUIT "$(< "$dir/logs/nginx.pid")"
