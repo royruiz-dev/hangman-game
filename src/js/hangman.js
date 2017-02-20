08d586953ddd57b1fa0d1540fdf7de45018507aa
Roy Ruiz <royruiz083@gmail.com>
2017-02-20 12:41:20 -0800

    Added function displayHangman() to show and hide the right parts of hangman.

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -9,45 +9,51 @@ var wordBank = "";
 var secretWord;
 var start, filter;
 
+//Set certain REST API parameters
 var difficulty = 7;
 var minLength = 4;
 var maxLength = 8;
 
+
 filter = filterWords(difficulty, minLength, maxLength);
 
+//Return a string for the url in GET method
 function filterWords(difficulty, minLength, maxLength) {
   return "&difficulty=" + difficulty + "&minLength=" + minLength + "&maxLength=" + maxLength;
 }
 
+
 function reqListener () {
   wordBank = this.responseText;
-  console.log(wordBank);
+  console.log(wordBank);  //Shows a bank of words
 }
 
 function fetchWord() {
   //http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words
   var xhttp = new XMLHttpRequest();
   xhttp.open("GET", "/words?" + filter, true);
-  xhttp.addEventListener('load', function(e) {
-     if (xhttp.readyState === 4) {
-       if (xhttp.status === 200) {
-         console.log(this.responseText);
-       } else {
-         console.error(this.statusText);
-     }
-   }
-   });
-  xhttp.onerror = function(e) {
-    console.error(this.statusText);
-  };
+  // xhttp.addEventListener('load', function(e) {
+  //    if (xhttp.readyState === 4) {
+  //      if (xhttp.status === 200) {
+  //        // console.log(this.responseText);
+  //      } else {
+  //        // console.error(this.statusText);
+  //    }
+  //  }
+  //  });
+  // xhttp.onerror = function(e) {
+  //   console.error(this.statusText);
+  // };
   xhttp.addEventListener("load", reqListener);
   xhttp.send(null);
 }
 
+//Randomize a word after the REST call execute and data in wordBank
 function randomizeWord() {
   var index = Math.floor(Math.random()*wordBank.split('\n').length);
   return wordBank.split('\n')[index];
 }
+
 
 var guess, valid;
 var wrongBin, correctBin = new Array;
@@ -55,6 +61,8 @@ var count, status, guessesAllowed;
 var regex = /^[a-zA-Z]$/;
 var display = "";
 
+
+//Display a set of underscores to use for later comparison
 function displayInitialize() {
   var display = "";
   for (i=0; i<secretWord.length; i++) display+="_";
@@ -68,19 +76,13 @@ function displayUnderscores() {
   word.innerHTML = displayBlanks;
 }
 
-//Prompt user to enter a guess letter
-function enterGuess() {
-  return prompt("Enter a letter!").toUpperCase();
+//Display Hangman as incorrect guesses appear
+function displayHangman(count) {
+    var list = document.getElementsByClassName('hangmanDude')[0];
+    list.getElementsByTagName('img')[count-1].style.display = "block";
 }
 
-function validateGuess(guess, correctBin, wrongBin, secretWord) {
-  if (!guess.match(regex)) return false; // Alert the format of the letter is wrong and User must enter letter from alphabet
-  else if (!/[a-zA-Z]/.test(secretWord)) return false;
-  else if (correctBin.indexOf(guess) > -1 || wrongBin.indexOf(guess) > -1) return false; // Alert the letter has already been used and tell User to enter another one
-  else return true;
-}
-
-//display word with correct guesses
+//Display word with correct guesses
 function displayString(secretWord, guess) {
   var letterBoxes = document.getElementsByClassName('letterBox');
   for (i=0; i<secretWord.length; i++) {
@@ -110,13 +112,20 @@ function guessesRemaining(count){
   guessesLeft.innerHTML = "Guesses remaining: <div class='guessCount'>" + (guessesAllowed - count) + "</div>";
 }
 
+//Perform guess and secret word validation
+function validateGuess(guess, correctBin, wrongBin, secretWord) {
+  if (!guess.match(regex)) return false; // Alert the format of the letter is wrong and User must enter letter from alphabet
+  else if (!/[a-zA-Z]/.test(secretWord)) return false;
+  else if (correctBin.indexOf(guess) > -1 || wrongBin.indexOf(guess) > -1) return false; // Alert the letter has already been used and tell User to enter another one
+  else return true;
+}
+
 //Determine status of the the game
 function gameStatus(count, display, secretWord) {
   if (count >= guessesAllowed) return -1;  // Computer wins
   else if (display === secretWord.toUpperCase()) return 1; // User wins
   else return 0;  //Let's keep playing
 }
-
 
 //Alert user wins
 function userWins() {
@@ -152,8 +161,14 @@ function resetGame() {
   document.getElementById('gameWinner').innerHTML = "";
   document.getElementById('wrongGuesses').innerHTML = "";
   document.getElementById('wordChallenge').innerHTML = "";
+
+  for (i=0; i<guessesAllowed; i++) {
+    var list = document.getElementsByClassName('hangmanDude')[0];
+    list.getElementsByTagName('img')[i].style.display = "none";
+  }
 }
 
+//Initializes a new game
 function initializeNewGame() {
   resetGame();
   secretWord = randomizeWord();
@@ -161,6 +176,7 @@ function initializeNewGame() {
   displayUnderscores();
 }
 
+//Main part of program where it loops until game winner decided
 function doGuess() {
   if (status !=0 ) return;  //inversion of while loop
 
@@ -178,6 +194,7 @@ function doGuess() {
 
       wrongGuess(wrongBin);
       guessesRemaining(count);
+      displayHangman(count);
     }
   status = gameStatus(count, display, secretWord);
   }
