189d397c5031dba9c654db5aa7271499f9fce110
Roy Ruiz <royruiz083@gmail.com>
2025-03-13 16:09:41 +0100

    Updated flask-app to run correctly on localhost

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,45 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from wonderwords import RandomWord
+import os
 
 app = Flask(__name__)
 r = RandomWord()
 
-def get_random_words(count, word_min_length=4, include_categories=None):
-  return r.random_words(count, word_min_length=word_min_length, include_categories=include_categories)
+BUILD_FOLDER = os.path.join(os.getcwd(), 'build')
 
-# generate random words meeting criteria below
-random_nouns = get_random_words(100, include_categories=["noun"])
-random_verbs = get_random_words(100, include_categories=["verb"])
+def get_random_words(count, min_length=4, max_length=10, categories=None):
+  return r.random_words(
+    count,                        # number of words to generate
+    word_min_length=min_length,   # minimum length of each word
+    word_max_length=max_length,   # maximum length of each word
+    include_categories=categories # filter words by categories
+  )
+
+# Generate random words based on specified category
+random_nouns = get_random_words(100, categories=["noun"])
+# random_verbs = get_random_words(100, include_categories=["verb"])
 
 @app.route('/nouns')
 def get_nouns():
   print("Headers: ", request.headers)
   return random_nouns
 
-@app.route('/verbs')
-def get_verbs():
-  print("Headers: ", request.headers)
-  return random_verbs
+# @app.route('/verbs')
+# def get_verbs():
+#   print("Headers: ", request.headers)
+#   return random_verbs
+
+# Serve the index.html from the build folder
+@app.route('/')
+def index():
+  return send_from_directory(BUILD_FOLDER, 'index.html')
+
+# Serves static files from the BUILD_FOLDER directory.
+# Matches any file request and returns the corresponding file.
+@app.route('/<path:filename>')
+def serve_static(filename):
+  return send_from_directory(BUILD_FOLDER, filename)
+
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True, port=5000)
