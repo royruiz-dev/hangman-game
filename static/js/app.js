c2066369d6c77361fabdde4c17a910df6e0f2c6a
Roy Ruiz <royruiz083@gmail.com>
2025-03-17 13:38:31 +0100

    Updated documentation and comments & fixed app.py to render static
    - Updated README.md to make it clearer and concise
    - Provided better code comments (.js and .sh files)
    - app.py now renders static files to run app on http://127.0.0.1:5000
    - Renamed main script to deploy.sh
    - Delete files no longer used

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,5 @@
-// For REST Call
-// set dificulty parameter value (1..10)
-// Pull word list count for specified difficulty value
-// Assign random number via Math.random method to START parameter
-// Assign count = 1 to return only 1 word
-// save word to secretWord string
-
-
-// filter = filterWords(difficulty, minLength, maxLength);
-
-// //Return a string for the url in GET method
-// function filterWords(difficulty, minLength, maxLength) {
-//   return "&difficulty=" + difficulty + "&minLength=" + minLength + "&maxLength=" + maxLength;
-// }
-
-//This function allows the XMLHttpRequest object to obtain the response from the server once it reaches a good readyState and status is clear to fetch. This is a robust way to perform request. After the response(long string value) is obtained by the object, it is saved in wordBank string and the game initializes by calling initializeGame()
-
-//If the server is not responding, then it will execute blockScreen() and game will not be started
+// Handles response from server once XMLHttpRequest is complete and status is successful
+// If the server is not responding, it calls blockScreen() and game is not started
 function reqListener() {
   if (this.readyState === 4) {
     if (this.status === 200) {
@@ -26,19 +10,15 @@ function reqListener() {
   }
 }
 
-//This function initializes an object with the document elements associated to id='errorOverlay' and then changes the CSS to show a blocked screen via 'display: block'. This functions is created for error handling
+// Displays an error overlay when server fails to respond
 function blockScreen() {
   var item = document.getElementById('errorOverlay');
   item.style.display = "block";
 }
 
-//Function that executes a REST call via the xMLHttpRequest object. First, it executes an open method to initialize a request. It runs the 'GET' method to fetch data pointing to the string concatenated by 'url + filter'. The third parameter is a Boolean determining this request will be asynchronous. Asynchronous request allows the browser to continue to be responsive while it requests the data. It appears to be recommended.
-
-//After fetching the data, an event handler runs as 'load' implementing the reqListener(). For error handling, two event handlers were added to also execute blockScreen() in the case there is and 'error' or 'abort' on the server-side
+// Makes an asynchronous REST call to fetch word data using XMLHttpRequest
 function fetchWord() {
-
   var xhttp = new XMLHttpRequest();
-  // xhttp.open("GET", url + filter, true);
   xhttp.open("GET", url , true);
   xhttp.addEventListener("load", reqListener);
   xhttp.addEventListener("error", blockScreen);
@@ -46,13 +26,15 @@ function fetchWord() {
   xhttp.send(null);
 }
 
-//This function determines the word count of wordBank array by calling 'length' property. It uses the count to provide a random number. A number is used to pull one array element, which becomes our secret word for the current game
+// Selects a random word from wordBank array
 function randomizeWord() {
   var index = Math.floor(Math.random() * wordBank.length);
   return wordBank[index];
 }
+// // This is then used as a comparison to the secretWord string and verify whether game is completed or not.
 
-//This function initializes a set of underscores according to the length of secretWord. This is then used as a comparison to the secretWord string and verify whether game is completed or not. The value of display can also be found in the console for debugging purposes.
+// Initializes a string of underscores based on the secret word length to track progress
+// The display value is also shown in the console for debugging purposes
 function displayInitialize() {
   var display = "";
 
@@ -60,20 +42,20 @@ function displayInitialize() {
   return display;
 }
 
-//This function initializes an object with the document elements associated to id='wordChallenge' and then displays a set of underscores equal to length of secretWord. It does this by calling the replace method where it looks for a letter and replaces each letter with some innerHTML. This shows the user how many blanks need to be guessed.
+// Displays underscores representing the 'unguessed' letters of the secret word
 function displayUnderscores() {
   var word = document.getElementById('wordChallenge');
   var displayBlanks = secretWord.replace(/./g,"<div class='letterBox'></div> ")
   word.innerHTML = displayBlanks;
 }
 
-//This function initializes an object with the document elements associated to class='letterBox'. This object is of Array type and used to pull 'img' tags within the class. It uses the count value, which corresponds to a value that increments as the user guesses incorrectly. In doing so, images of the Hangman silhouette are displayed by changing its CSS to 'display: block' one incorrect guess at a time
+// Displays the hangman image based on the number of incorrect guesses
 function displayHangman(count) {
     var list = document.getElementsByClassName('hangmanDude')[0];
     list.getElementsByTagName('img')[count-1].style.display = "block";
 }
 
-//This function initializes an object (Array type) with the document elements associated to class='letterBox'. Because this object is an array, a for loop is used to check where the guess letter matches the character position of secretWord. innerHTML is used to enter the guess letter in the correct position of the secret word. Display is also being tracked again for comparison and debugging purposes.
+// Displays the guessed letter in the correct position of the secret word
 function displayString(secretWord, guess) {
   var letterBoxes = document.getElementsByClassName('letterBox');
   for (i=0; i<secretWord.length; i++) {
@@ -85,57 +67,54 @@ function displayString(secretWord, guess) {
   return display;
 }
 
-//This function is used to push the guess letter into 'correctBin' array to keep track of correct guesses. We don't show it in the interface since we already show the correct letters in the HTML; however, this was created for debugging purposes.
+// Adds the correct guessed letter to the correctBin array for debugging
 function rightGuess(guess) {
   correctBin.push(guess);
 }
 
-//This function initializes an object with the document elements associated to id='wrongGuesses'. A map function is called where it returns each element in wrongBin array contained by the HTML below. Finally, a innerHTML is used to enter the incorrect guesses in the left panel
+// Displays list of incorrect guessed letters
 function wrongGuess(wrongBin) {
   var wrongBucket = document.getElementById('wrongGuesses');
   var displayWrongGuesses = wrongBin.map(function(x){return "<div class='wrongLetter'>" + x + "</div>"}).join(', ');
   wrongBucket.innerHTML = "<b>Failed Guesses:</b> " + displayWrongGuesses;
 }
 
-//This function does a similar execution to wrongGuess(); however, it's simpler because it is not dealing with an array. Instead, it uses the count value incremented every time there is an incorrect guess to run innerHTML and output in the DOM the guesses remaining to the user playing.
+// Displays number of remaining guesses
 function guessesRemaining(count){
   var guessesLeft = document.getElementById('guessesLeft');
   guessesLeft.innerHTML = "<b>Attempts Left:</b> <div class='guessCount'>" + (guessesAllowed - count) + "</div>";
 }
 
-//This function validates the letter (guess) being clicked on has not been saved as an incorrect or correct guess. If it has, then one of the conditions in line 108 will output true, which then returns false and wants the user to select another letter for guess. If it does not meet any of the conditions, then the function returns true and continues to next line of code.
+// Validates the guessed letter has not been used before (either correct or incorrect)
 function validateGuess(guess, correctBin, wrongBin, secretWord) {
-  if (!guess.match(/^[a-zA-Z]$/)) return false; // Checks the format of the letter is wrong and User must enter letter from alphabet. This was used when a prompt was executed and needed to check the letter format was correct.
-  else if (!/[a-zA-Z]/.test(secretWord)) return false; //checks secretWord is not an empty string.
-  else if (correctBin.indexOf(guess) > -1 || wrongBin.indexOf(guess) > -1) return false; // Alert the letter has already been used and tell User to enter another one
+  if (!guess.match(/^[a-zA-Z]$/)) return false; // Ensures the guess is a valid letter
+  else if (!/[a-zA-Z]/.test(secretWord)) return false; // Checks secret word is not empty
+  else if (correctBin.indexOf(guess) > -1 || wrongBin.indexOf(guess) > -1) return false; // Ensures letter has not been guessed before
   else return true;
 }
 
-//At every turn, this function checks the game status to see whether the user or computer has won. If not, it returns 0 to keep the party going!
+// Checks game status: (1) if user or computer has won, or (2) if game is still ongoing
 function gameStatus(count, display, secretWord) {
-  if (count >= guessesAllowed) return -1;  // Computer wins
+  if (count >= guessesAllowed) return -1; // Computer wins
   else if (display === secretWord.toUpperCase()) return 1; // User wins
-  else return 0;  //Let's keep playing
+  else return 0; // Game continues
 }
 
-//Once gameStatus() returns a value that shows user wins, a innerHTML is output to the DOM to say 'Guesser Wins!' Note the return value of gameStatus() is executed at every turn and saved in 'status' variable.
+// Displays 'PLAYER WINS!' message when user wins
 function userWins() {
   var winner = document.getElementById('gameWinner');
-  winner.innerHTML = "GUESSER WINS!";
-  // Element visible after winner is announced
-  winner.style.display = 'flex';
+  winner.innerHTML = "PLAYER WINS!";
+  winner.style.display = 'flex'; // Makes the winner message visible
 }
 
-//Once gameStatus() returns either a value that shows computer wins, a innerHTML is output to the DOM to say 'Secret Keeper Wins!'. Note the return value of gameStatus() is executed at every turn and saved in 'status' variable.
+// Displays 'SECRET KEEPER WINS!' message when computer wins
 function userLoses() {
   var winner = document.getElementById('gameWinner');
   winner.innerHTML = "SECRET KEEPER WINS!";
-  // Element visible after winner is announced
-  winner.style.display = 'flex';
+  winner.style.display = 'flex'; // Makes the winner message visible
 }
 
-
-//This function eexceutes within initializeNewGame() to reset all necessary values to be ready for a new game. It also clears all innerHTML manipulated throughout the game when playing. At the start of a game, the guesses remaining appears because it is assumed the game already started and lets the user know he/she has six guesses remaining
+// Resets necessary values for a new game and clears previous game data from the DOM
 function resetGame() {
   status = 0;
   count = 0;
@@ -149,18 +128,18 @@ function resetGame() {
   document.getElementById('wrongGuesses').innerHTML = "";
   document.getElementById('wordChallenge').innerHTML = "";
 
-  // Hide winner display 'flex' block
+  // Hide the winner display
   var winner = document.getElementById('gameWinner');
   winner.style.display = 'none';
 
-  //A for loop is used to initialize since the 'list' object is of Array type and each element within the array must be initialized.
+  // Hides hangman images
   for (i=0; i<guessesAllowed; i++) {
     var list = document.getElementsByClassName('hangmanDude')[0];
     list.getElementsByTagName('img')[i].style.display = "none";
   }
 }
 
-//This function executes the functions noted below at the start of every new game.
+// Initializes a new game by resetting the game and selecting a new word
 function initializeNewGame() {
   resetGame();
   secretWord = randomizeWord();
@@ -168,9 +147,9 @@ function initializeNewGame() {
   displayUnderscores();
 }
 
-//This function executes every time a letter in class='alphabet' is handled by a 'click' event. This function executes when DOM Content has been loaded and it executes at every turn until a game winner has been decided. Most of the function calls explained above occur here!
+// Handles the 'guessing' process during the game, checking if guess is correct or incorrect
 function doGuess() {
-  if (status !=0 ) return;  //inversion of while loop
+  if (status !=0 ) return;  // Prevents guessing once games has ended
 
   guess = this.innerText;
   valid = validateGuess(guess, correctBin, wrongBin, secretWord);
@@ -188,21 +167,22 @@ function doGuess() {
       guessesRemaining(count);
       displayHangman(count);
     }
-  status = gameStatus(count, display, secretWord);
+  status = gameStatus(count, display, secretWord); // Stores status value
   }
 
-  //For debugging purposes only. See explanation below above its function call.
-  consoleOutput(display, secretWord, wrongBin, correctBin, count);
+  // Used for debugging purposes and logs the game status
+  consoleOutput(display, secretWord, wrongBin, correctBin, count, status);
 
   if (status == 1) userWins();
   else if (status == -1) userLoses();
 }
 
-//This is strictly for debugging purposes only and is not to be included as part of the game code. I have it here, so I can see what is the console outputting as I go through the game.
-function consoleOutput(display, secretWord, wrongBin, correctBin, count) {
+// Game information logs relevant for debugging
+function consoleOutput(display, secretWord, wrongBin, correctBin, count, status) {
   console.log("Display is: "+ display);
   console.log("Secret word is: " + secretWord);
   console.log("Wrong Guesses: " + wrongBin);
   console.log("Correct Guesses: " + correctBin);
   console.log("Misses: " + count);
+  console.log("Status: " + status);
 }
--- static/js/config.js
+++ static/js/config.js
@@ -1,22 +1,24 @@
-//Parameters for REST API call executions
-// var difficulty = 6;  //set to return words based on dificulty level
-// var minLength = 4;  //set to return words longer than the provided length
-// var maxLength = 8;  //set to return words shorter than the provided length
+// URL endpoint t fetch word data from API (e.g., a list of nouns)
+var url = "/nouns";
 
-//http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words
-//Due to CORS issue, the application is proxying to the domain above via nginx defined in 'proxy.conf' file in the 'config' folder. This way the REST API can still be executed via GET method
-// var url = "/words?"
-var url = "/nouns"
+// Initializes string to store response from REST API
+// Used to select a random word for the game
+var wordBank = "";
 
-var wordBank = "";  //initialize string variable to store the value returned by REST call. The return value is used to output a random word every time new game is executed
-var secretWord;  //variable used to store string value returned by REST call
-// var filter;   //variable used to store string value needed to concatenate with url to pull a subset of words via REST call
+// Stores selected word returned from REST API
+var secretWord;
 
 var guess;  //assigned to innerText property where it reads the string value within 'alphabet' class when executing doGuess()
 var valid;  //assigned to boolean return from validateGuess(). This boolean value tells us whether the guess being passed is valid for the game.
 
-var wrongBin, correctBin = new Array;  //initializes the arrays used to store the correct and incorrect guesses. The user interface outputs the incorrect guess during application run; however, the console uses the correct guesses for debugging purposes.
+// Initializes arrays for correct and incorrect guesses
+// Incorrect guesses are displayed in the UI; correct ones are used for debugging
+var wrongBin, correctBin = new Array;
 
-var count, status, guessesAllowed;  //these variables are initialized in resetGame(). They are used to track the number of incorrect guesses, guesses remaining, and whether we have a game winner or keep playing.
+// To track incorrect guesses, remaining guesses, and game status
+// These variables are reset in resetGame()
+var count, status, guessesAllowed;
 
-var display = "";  //string variable is used to validate competion of secret word by user
+// Used to track current state of secret word,
+// Helps validate whether the user has completed guessing the secret word
+var display = "";
